337e94dbf42a150890c862eee961bccfb13997b2
rohit sutar <rohitsureshsutar2000@gmail.com>
2023-12-22 16:26:22 +0530

    PaintBrush

---
+++ Day3/PaintBrush/Form1.cs
@@ -0,0 +1,27 @@
+namespace PaintBrush;
+using DrawShapes;
+using System.Drawing;
+public partial class Form1 : Form
+{
+    DrawShapes.Point StartPoint;
+    DrawShapes.Point EndPoint;
+    
+    public Form1()
+    {
+        MouseDown += new System.Windows.Forms.MouseEventHandler(onMouseDown);
+        MouseUp+= new System.Windows.Forms.MouseEventHandler(onMouseUp);
+        InitializeComponent();
+    }
+
+    public void onMouseDown(object sender,MouseEventArgs e){
+        StartPoint=new DrawShapes.Point(e.X,e.Y);
+    }
+    public void onMouseUp(object sender,MouseEventArgs e){
+        EndPoint=new DrawShapes.Point(e.X,e.Y);
+        Line l=new Line(StartPoint,EndPoint,"red",3);
+        Pen pen=new Pen(Color.FromArgb(255,255,0,0));
+        Graphics g=this.CreateGraphics();
+        g.DrawLine(pen,l.StartPoint.X,l.StartPoint.Y,l.EndPoint.X,l.EndPoint.Y);
+    }
+
+}
