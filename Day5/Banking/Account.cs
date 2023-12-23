43b44bbc0b80513e0be5ffe0dc17e0d3a792f3d8
rohit sutar <rohitsureshsutar2000@gmail.com>
2023-12-23 14:23:09 +0530

    sln file, delegate and event

---
+++ Day5/Banking/Account.cs
@@ -0,0 +1,11 @@
+namespace Bank;
+class Account{
+    public int Balance{
+        get;set;
+    }
+    Account()
+    {
+        this.Balance=
+    }
+
+}
---
+++ Day5/Banking/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Banking;
+
+public class Class1
+{
+
+}
