ad31185dd322b1ac4a47c305a28bec360960aad6
rohit sutar <rohitsureshsutar2000@gmail.com>
2023-12-25 15:37:40 +0530

    Explicit interface Inheritance

--- Day6/Explicit_Intf_Inheri/Combine.cs
+++ Day6/Explicit_Intf_Inheri/Combine.cs
@@ -1,5 +1,21 @@
 namespace ExpliInheri;
  
 public class inheri:emp,orderDetails{
-    
+    void sayHello()
+    {
+        Console.log("Hello");
+    }
+    void sayGoodMorning()
+    {
+        Console.log("Good Morning");
+
+    }
+    void showDetails()
+    {
+        Console.log("in show Details");
+    }
+    void showDetails()
+    {
+        Console.log("in show Details");
+    }
 }
