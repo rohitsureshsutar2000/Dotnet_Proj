ea439d5f269179a3a4f8c956e72aa50d175b6843
rohit sutar <rohitsureshsutar2000@gmail.com>
2023-12-26 15:17:44 +0530

    Open Database Connectivity

---
+++ Day7/Dbconnection/Program.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+MySqlConnection connection=new MySqlConnection();
+connection.ConnectionString="server=localhost;port=3306;user=root;password=welcome;database=dotnetPract";   //CURD
+if(connection!=null )
+{
+    Console.WriteLine(" Connecction Successfull");
+}
+
+// string query="create table testing(id int,name varchar(20))";     // create table
+// string query="update emp set ename='sayali' where empno=6001";    // update table
+string query="select empno,ename from emp";                         // read table
+// string query="delete from emp where empno=7840";                 // Delete data
+MySqlCommand command=new MySqlCommand(query,connection);
+
+try
+{
+    connection.Open();
+
+    
+    // command.ExecuteReader();                             =DDL command            // create
+    // command.ExecuteNonQuery();               //          =DML command            //  update
+    MySqlDataReader reader=command.ExecuteReader();     //  =DDl command            // read
+            while(reader.Read())
+            {
+                Console.Write(reader["empno"].ToString()+" ");
+                Console.WriteLine(reader["ename"].ToString());
+            }
+    // command.ExecuteNonQuery();           =DML command           //  delete
+}
+catch (Exception e) 
+{
+    Console.WriteLine(e.Message);
+}
+finally{
+    connection.Close();
+}
