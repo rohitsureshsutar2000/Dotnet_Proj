3f29fb4b8d5658b14bc4f3b19a1237a97dcadf5c
rohit sutar <rohitsureshsutar2000@gmail.com>
2023-12-30 19:14:22 +0530

    curd op of riderpoint object

---
+++ Day11/projMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace projMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Day11/projMVC/Models/Rider_details.cs
@@ -0,0 +1,15 @@
+namespace BOJ;
+public class Rider_details{
+    public int Rider_Id{
+        get;set;
+    }
+     public string Name{
+        get;set;
+    }
+     public string Mobile_no{
+        get;set;
+    }
+     public string Address{
+        get;set;
+    }
+}
